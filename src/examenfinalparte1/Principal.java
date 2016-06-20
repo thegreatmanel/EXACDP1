66ddfdf930a3cfbba181a05adad094ebeeef613b
juchafernandez <juchafernandez@atanasoff05.danielcastelao.org>
2016-06-20 09:18:17 +0200

    variables y main renombrada. acabado getter. solucionado error de c�digo en sumas

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,9 +1,13 @@
 package examenfinalparte1;
+import java.io.IOException;
 public class Principal {
-    int i;
-public Principal(int j, int m) {i=met(j,m);}
-    public Principal(int j) {
-        i=met(j,j);}
-    public Principal() { i=-1;
-    }public int met(int x, int x1) {
-      return x + x1;}}
+public static void main(String[] args) throws IOException{
+Sumas suma1 = new Sumas(10,20);
+int resultadosuma1 = suma1.getResultado();
+System.out.println("Es: " + resultadosuma1);
+Sumas suma2 = new Sumas(10);
+int resultadosuma2 = suma2.getResultado();
+System.out.println("Es: " + resultadosuma2);
+Sumas suma3 = new Sumas();
+int resultadosuma3 = suma3.getResultado();
+System.out.println("Es: " + resultadosuma3);}}
--- src/examenfinalparte1/Sumas.java
+++ src/examenfinalparte1/Sumas.java
@@ -1,8 +1,8 @@
 package examenfinalparte1;
 public class Sumas {
     private int resultado;
-    public int getResultado();
-    return resultado;
+    public int getResultado(){
+    return resultado;}
 public Sumas(int numero1, int numero2) {resultado=sumar(numero1,numero2);}
     public Sumas(int num) {
         resultado=sumar(num,num);}
