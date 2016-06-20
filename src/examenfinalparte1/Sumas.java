7e0205a5f22aeec067c5f5ae63fe3533cf0cdf6d
juchafernandez <juchafernandez@atanasoff05.danielcastelao.org>
2016-06-20 09:09:41 +0200

    variables, clases y m�todos renombrados

---
+++ src/examenfinalparte1/Sumas.java
@@ -0,0 +1,11 @@
+package examenfinalparte1;
+public class Sumas {
+    private int resultado;
+    public int getResultado();
+    return resultado;
+public Sumas(int numero1, int numero2) {resultado=sumar(numero1,numero2);}
+    public Sumas(int num) {
+        resultado=sumar(num,num);}
+    public Sumas() { resultado=-1;
+    }public int sumar(int numero1, int numero2) {
+      return numero1 + numero2;}}
