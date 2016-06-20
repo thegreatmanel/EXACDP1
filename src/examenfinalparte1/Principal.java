3298d4d92c8cf7cdafd40a6bae274ff0d29e0d64
juchafernandez <juchafernandez@atanasoff05.danielcastelao.org>
2016-06-20 09:25:12 +0200

    javadoc añadido a la main

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,5 +1,9 @@
 package examenfinalparte1;
 import java.io.IOException;
+/**
+ * clase encargada de imprimir los datos por pantalla
+ * @author juchafernandez
+ */
 public class Principal {
 public static void main(String[] args) throws IOException{
 Sumas suma1 = new Sumas(10,20);
